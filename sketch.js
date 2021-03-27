eb2862ffcd8d6c46e4aac928b6a25ced44743241
calvinH567 <76797277+calvinH567@users.noreply.github.com>
2021-03-26 22:16:58 -0700

    added comments

--- sketch.js
+++ sketch.js
@@ -27,6 +27,7 @@ function setup() {
   spaceShip = createSprite(400,400,25,25,);
   asteroid.velocityY = 0;
   
+  
   asteroidScatter1 = new rock(Math.round(random(0,400)),0,asteroidHealth/10,1);
   asteroidScatter2 = new rock(Math.round(random(0,400)),0,asteroidHealth/10,2);
   asteroidScatter3 = new rock(Math.round(random(0,400)),0,asteroidHealth/10,3);
@@ -39,40 +40,47 @@ function draw() {
       ability1Cooldown -=0.1;
     }else{
       if(keyDown("1")){
+        //effect of ability1; knock everything back slightly
         asteroid.velocityY = -3;
         asteroidScatter1.y = -10;
         asteroidScatter2.y = -10;
         asteroidScatter3.y = -10;
         ability1Cooldown = 10;
-        ability1Used = ability1Used + 1;
+        ability1Used = ability1Used + 1;//used to calculate damage increase
       }
     }
     if(ability2cooldown>0){
       ability2cooldown-=0.1;
     }else{
       if(keyDown("2")){
+        //effect of ability2; teleport everything back 
         ability2cooldown=20;
         asteroid.y = 0;
         asteroidScatter1.y = -10;
         asteroidScatter2.y = -10;
         asteroidScatter3.y = -10;
-        ability2Used = ability2Used + 1;
+        ability2Used = ability2Used + 1;//used to calculate damage gain
       }
     }
     if(asteroid.y>800||asteroidScatter1.y>800||asteroidScatter2.y>800||asteroidScatter3.y>800){
       gameState = 0;
     }
     if(asteroidHealth<1){
-     
+      //how asteroid hp is calculated
       asteroidHealth = waveMultiplier*currentWave;
+
+      //adds more multiplier every 10 waves
       if(currentWave%10 == 0){
         waveMultiplier = waveMultiplier + 5;
       }
+
+      //based on amount of abilities used in wave, it will increases damage
       asteroid.y = 0;
       abilityTotal = ability1Used + ability2Used;
       damage = damage + 1000/abilityTotal;
-    }else{
 
+    }else{
+      //resets all asteroids scatters (unused)
       asteroidScatter1.y = 0;
       asteroidScatter1.health = asteroidHealth /10;
 
@@ -81,26 +89,27 @@ function draw() {
 
       asteroidScatter3.y = 0;
       asteroidScatter3.health = asteroidHealth /10;
+      //resets all asteroids scatters (unused)
 
       if(scattershotDown >2){
-         currentWave = currentWave + 1;
-         acceleration = acceleration + 0.05;
-         scattershotDown = 0;
+         currentWave = currentWave + 1;//-------increases difficulty of hp and speed of asteroids
+         acceleration = acceleration + 0.05;//speed difficulty increases
+         scattershotDown = 0;//resets so it doesn't go up as much
       }
     }
     if(asteroid.velocityY >=acceleration){
-
+      //only used for the else function
     }else{
-      asteroid.velocityY = asteroid.velocityY+0.1
+      asteroid.velocityY = asteroid.velocityY+0.1//gravitational pull towards earth
     }
     if(spaceShip.isTouching(asteroid)){
       cooldownCounter = cooldownCounter -0.1;
-      if(cooldownCounter <0){
-        cooldownCounter=cooldown
+      if(cooldownCounter <0){//----cooldown for attack(spaceship must be touching                                
+        cooldownCounter=cooldown//// the asteroid for cooldown to go down)
         asteroidHealth = asteroidHealth - damage;
       }
     
-    }
+    }//drag movement
     spaceShip.x = mouseX;
     spaceShip.y = mouseY;
     
@@ -108,18 +117,22 @@ function draw() {
     background(0);  
     drawSprites();
     //image(50,50,50,50,spaceshipIMG)
-    
+
+    //tutorial
     text("When an ability pops up, use the 1/2 keys to activate it",10,25);
     text("Hover the space ship over the asteroid",10,10);
 
+    //displays hp
     fill("white")
     text(asteroidHealth,asteroid.x,asteroid.y);
     push ()
-    
+
+    //shows when ability is ready
     if(ability1Cooldown <=0){stroke("blue")}else{stroke("black")}
     fill (rgb(ability1Cooldown*25,ability1Cooldown*25,ability1Cooldown*25));
     rect (200,700,50,50);
-    
+
+    //shows when ability is ready
     if(ability2cooldown <=0){stroke("blue")}else{stroke("black")}
     fill(rgb(ability2cooldown*12.5,ability2cooldown*12.5,ability2cooldown*12.5));
     rect (600,700,50,50);
@@ -129,6 +142,8 @@ function draw() {
     asteroidScatter2.draw();
     asteroidScatter3.draw();
 }else{
+
+  //defeat text
   fill ("white");
   textSize(25)
   text ("The earth was destroyed! You survived for "+currentWave+" years",50,400);
